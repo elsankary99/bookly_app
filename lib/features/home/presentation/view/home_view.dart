acdf13cecf2f15982f8523ec6b3f940a04727bf7
Ahmed Elsankary <elsankary99@gmail.com>
2023-03-01 19:47:51 +0200

    create custom app bar

--- lib/features/home/presentation/view/home_view.dart
+++ lib/features/home/presentation/view/home_view.dart
@@ -9,7 +9,6 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      backgroundColor: Colors.red,
       body: HomeViewBody(),
     );
   }
--- lib/features/home/presentation/view/widget/home_view_body.dart
+++ lib/features/home/presentation/view/widget/home_view_body.dart
@@ -1,6 +1,10 @@
+import 'package:bookly_app/core/utils/assets.dart';
+import 'package:bookly_app/features/home/presentation/view/widget/custom_app_bar.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -8,7 +12,7 @@ class HomeViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: [],
+      children: const [CustomAppBar()],
     );
   }
 }
