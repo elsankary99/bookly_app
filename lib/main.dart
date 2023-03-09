322c11395e2451de21428da399bb3daeae0c41fd
Ahmed Elsankary <elsankary99@gmail.com>
2023-03-09 23:31:06 +0200

    creat setup Server Locator

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
 import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/core/utils/app_routers.dart';
+import 'package:bookly_app/core/utils/srvice_locator.dart';
 import 'package:bookly_app/features/Splash/presentation/view/splash_view.dart';
+import 'package:bookly_app/features/home/data/repos/home_repo_impl.dart';
+import 'package:bookly_app/features/home/presentation/manger/featuerd_books_cubit/featuerd_books_cubit.dart';
+import 'package:bookly_app/features/home/presentation/manger/newset_books_cubit/newset_books_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get/get_navigation/get_navigation.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
+  setupServerLocator();
   runApp(const BooklyApp());
 }
 
@@ -14,13 +20,27 @@ class BooklyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: AppRouter.router,
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: kPrimaryColor,
-        textTheme: GoogleFonts.montserratTextTheme(
-          ThemeData.dark().textTheme,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => FeatuerdBooksCubit(
+            getIt.get<HomeRepoImpl>(),
+          ),
+        ),
+        BlocProvider(
+          create: (context) => NewsetBooksCubit(
+            getIt.get<HomeRepoImpl>(),
+          ),
+        ),
+      ],
+      child: MaterialApp.router(
+        routerConfig: AppRouter.router,
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: kPrimaryColor,
+          textTheme: GoogleFonts.montserratTextTheme(
+            ThemeData.dark().textTheme,
+          ),
         ),
       ),
     );
