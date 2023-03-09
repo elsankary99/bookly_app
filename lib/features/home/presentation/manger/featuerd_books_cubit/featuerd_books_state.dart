a71e4e536908225ff909ca97c1e799395eb7ae9a
Ahmed Elsankary <elsankary99@gmail.com>
2023-03-09 22:16:37 +0200

    create featuer books cubit

---
+++ lib/features/home/presentation/manger/featuerd_books_cubit/featuerd_books_state.dart
@@ -0,0 +1,24 @@
+part of 'featuerd_books_cubit.dart';
+
+abstract class FeatuerdBooksState extends Equatable {
+  const FeatuerdBooksState();
+
+  @override
+  List<Object> get props => [];
+}
+
+class FeatuerdBooksInitial extends FeatuerdBooksState {}
+
+class FeatuerdBooksLoading extends FeatuerdBooksState {}
+
+class FeatuerdBooksFailuer extends FeatuerdBooksState {
+  final String errMessage;
+
+  const FeatuerdBooksFailuer(this.errMessage);
+}
+
+class FeatuerdBooksSuccess extends FeatuerdBooksState {
+  final List<BookModel> books;
+
+  const FeatuerdBooksSuccess(this.books);
+}
