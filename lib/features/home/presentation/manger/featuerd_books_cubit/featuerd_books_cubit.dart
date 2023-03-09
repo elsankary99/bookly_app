54153b9dcb1d75c811c4a3c31e57d679ed6137f8
Ahmed Elsankary <elsankary99@gmail.com>
2023-03-09 22:30:43 +0200

    craete Featuerd Books method

--- lib/features/home/presentation/manger/featuerd_books_cubit/featuerd_books_cubit.dart
+++ lib/features/home/presentation/manger/featuerd_books_cubit/featuerd_books_cubit.dart
@@ -1,9 +1,21 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly_app/features/home/data/model/book_model/book_model.dart';
+import 'package:bookly_app/features/home/data/repos/home_repo_impl.dart';
 import 'package:equatable/equatable.dart';
 
 part 'featuerd_books_state.dart';
 
 class FeatuerdBooksCubit extends Cubit<FeatuerdBooksState> {
-  FeatuerdBooksCubit() : super(FeatuerdBooksInitial());
+  FeatuerdBooksCubit(this.homeRepoImpl) : super(FeatuerdBooksInitial());
+  final HomeRepoImpl homeRepoImpl;
+  Future<void> fetchFeatuerdBooks() async {
+    emit(FeatuerdBooksLoading());
+
+    var result = await homeRepoImpl.fetchFeatuerdBooks();
+    result.fold((failuer) {
+      emit(FeatuerdBooksFailuer(failuer.errMessage));
+    }, (books) {
+      emit(FeatuerdBooksSuccess(books));
+    });
+  }
 }
